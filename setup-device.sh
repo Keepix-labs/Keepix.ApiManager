c883caf39dde2e9bd7136b0c79a1ecdf826b2400
jguyet <gygydu19@hotmail.fr>
2023-09-22 13:27:15 +0200

    update interface

--- setup-device.sh
+++ setup-device.sh
@@ -1,4 +1,8 @@
-apt-get update
-apt-get upgrade
+apt-get --yes update
+apt-get --yes upgrade
 apt-get --yes install git ansible build-essential wireless-tools hostapd dnsmasq python3-dev python3-pip
 pip3 install pyaccesspoint
+curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
+source ~/.bashrc
+nvm install v18.18.0
+git clone https://github.com/Keepix-labs/Keepix.ApiManager.git ~/Keepix.ApiManager
