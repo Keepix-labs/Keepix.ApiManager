0909845fddbc9115512a3f7abd5c4c5a8dbc76c3
jguyet <gygydu19@hotmail.fr>
2023-09-26 22:23:27 +0200

    add cors

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,18 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+  });
+
+  app.enableCors({
+      allowedHeaders:"*",
+      origin: "*"
+  });
+
   await app.listen(80, "0.0.0.0"); // run api server
   app.get(ApiService).schedule(); // run api Scheduler
 }
