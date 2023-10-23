4a942f4785f28b2e332bc906f7b8800e88242948
jguyet <gygydu19@hotmail.fr>
2023-10-23 08:19:40 +0200

    fix create release script

--- publish-create-new-release.sh
+++ publish-create-new-release.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ $1 =~ ^v([\\d])* ]]; then
-    tar -czvf "./dist/api.tar.gz" -C "./dist"
+    tar -czvf "./dist/api.tar.gz" "./dist"
     gh release create "$1" ./dist/*.tar.gz
 else
     echo "Please specify a version arg0 vX.X.X"
