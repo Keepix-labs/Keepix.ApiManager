6436985751590f2aa7664f0aeae147e42f3a4a18
jguyet <gygydu19@hotmail.fr>
2023-10-29 14:04:09 +0100

    clean code

--- setup-device.sh
+++ setup-device.sh
@@ -8,18 +8,7 @@ apt-get --yes install git ansible build-essential wireless-tools hostapd dnsmasq
 curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash 
 source ~/.bashrc
 nvm install v18.18.0
-# git clone https://github.com/Keepix-labs/Keepix.ApiManager.git ~/Keepix.ApiManager
 npm install -g pm2
-# cd ~/Keepix.ApiManager
-# npm install
 
-mkdir /root/.keepix
-wget -O /root/.keepix/api.tar.gz https://github.com/Keepix-labs/Keepix.ApiManager/releases/download/v0.0.37/api.tar.gz
-rm -rf /root/.keepix/release
-tar -xvf /root/.keepix/api.tar.gz -C /root/.keepix
-rm -rf /root/.keepix/api.tar.gz
-cp -r /root/.keepix/release/ssl /root/.keepix/ssl
-cp /root/.keepix/release/package.json /root/.keepix/package.json
-cp /root/.keepix/release/run.js /root/.keepix/run.js
-npm install --prefix /root/.keepix
-pm2 restart /root/.keepix/release/pm2.config.js
+npm install -g keepix-server-build@latest
+keepix-server pm2
